d79be8eed3447f78a7df82fccafdad9e99b422c8
Jonathan Leaf <jcleaf@Megans-MacBook-Pro.local>
2018-01-27 10:23:24 -0800

    line of sight AI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,25 +12,31 @@ public class Enemy : MonoBehaviour {
 		player = GameObject.Find("Player");
 		self = GetComponent<GameObject>();
 		nav = GetComponent<NavMeshAgent>();
-		playerDetected = false;
+		playerDetected = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// If the enemy and the player have health left...
-		if(playerDetected)
+		if(LineOfSight(player.transform) && playerDetected)
 		{
 			// ... set the destination of the nav mesh agent to the player.
 			nav.SetDestination (player.transform.position);
+//			Vector3 distance = (player.transform.position - transform.position);
+
+//			nav.nextPosition = (nav.speed*Time.deltaTime)*distance.normalized;
 		}
 		// Otherwise...
 		else
 		{
 			// ... disable the nav mesh agent.
-			if(self != null)
-				nav.SetDestination (self.transform.position);
+			if (self != null)
+				
+				nav.velocity = Vector3.zero;
+				nav.SetDestination (transform.position);
 		}
 	}
+	/*
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.name == "Player") {
 			playerDetected = true;
@@ -40,5 +46,17 @@ public class Enemy : MonoBehaviour {
 		if (col.gameObject.name == "Player") {
 			playerDetected = false;
 		}
+	}
+	*/
+	double fov = 360.0;
+	private RaycastHit hit;
+
+	bool LineOfSight(Transform target) {
+		if (Vector3.Angle(target.position - transform.position, transform.forward) <= fov &&
+			Physics.Linecast(transform.position, target.position, out hit) &&
+			hit.collider.transform == target) {
+			return true;
+		}
+		return false;
 	}
 }
