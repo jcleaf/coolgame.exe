ef1c4e7dfd8123a7908a67c79199bd0f478adacb
Jonathan Leaf <jcleaf@Megans-MacBook-Pro.local>
2018-01-27 11:17:31 -0800

    beacon activation and enemy logic

---
+++ Assets/Scripts/Beacon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+public class Beacon : MonoBehaviour {
+	public bool activated;
+	Enemy enemy;
+	// Use this for initialization
+	void Start () {
+		activated = true;
+		enemy = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void beaconActivation(bool activation){
+		activated = activation;
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "Enemy" && activated) {
+			enemy = col.gameObject.GetComponent<Enemy>();
+			enemy.playerDetected = true;
+		}
+	}
+
+}
