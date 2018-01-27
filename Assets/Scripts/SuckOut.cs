f59726873c20d9a55e367ea1eca577a5d891c333
Caleb Markley <cablay91@gmail.com>
2018-01-27 15:01:49 -0800

    Exit prototyping.

---
+++ Assets/Scripts/SuckOut.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SuckOut : MonoBehaviour
+{
+    public float suckStrength;
+    public AnimationCurve distanceMultiplierCurve;
+    private float triggerSize;
+
+    void Awake()
+    {
+        SphereCollider triggerCollider = GetComponent<SphereCollider>();
+        triggerSize = triggerCollider.radius * triggerCollider.transform.lossyScale.x;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody)
+        {
+            Vector3 offset = transform.position - other.transform.position;
+            Vector3 suckDir = offset.normalized;
+            float distanceMutliplier = distanceMultiplierCurve.Evaluate(offset.sqrMagnitude / (triggerSize * triggerSize));
+            other.attachedRigidbody.AddForce(suckDir * suckStrength * distanceMutliplier * Time.fixedDeltaTime);
+        }
+    }
+}
