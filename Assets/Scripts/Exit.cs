74f6268b357a41ca38d3d5a1890b9a0b07b551f7
Caleb Markley <cablay91@gmail.com>
2018-01-27 15:45:34 -0800

    Exit stuff.

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,11 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
+    [SerializeField] private bool openCheckbox;
+    [SerializeField] private BoxCollider doorCollider;
+
+    [HideInInspector] public Level levelManager;
+
+    private bool open;
+    private GameObject suckOut;
+
+    void Awake()
+    {
+        suckOut = GetComponentInChildren<SuckOut>().gameObject;
+        suckOut.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (open != openCheckbox)
+        {
+            ToggleOpenState(openCheckbox);
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
+        if (!open)
+        {
+            return;
+        }
+
         other.gameObject.layer = LayerMask.NameToLayer("Debris");
+
+        if (other.gameObject.tag == "Player" && levelManager.levelFinished)
+        {
+            //TODO: YOU WIN!!!
+        }
+    }
+
+    public void Open() { ToggleOpenState(true); }
+
+    public void Close() { ToggleOpenState(false); }
+
+    private void ToggleOpenState(bool open)
+    {
+        this.open = open;
+        openCheckbox = open;
+
+        suckOut.SetActive(open);
+        doorCollider.gameObject.SetActive(!open);
     }
 }
