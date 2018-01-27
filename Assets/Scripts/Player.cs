1bed61231277aecd26d3822afb41d0883ba5a648
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-01-26 21:38:51 -0800

    Add player, tweak input settings

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private CharacterController _controller;
+	public float Speed = 2f;
+
+	void Start() {
+		_controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	    _controller.Move(move * Time.deltaTime * Speed);
+	}
+}
