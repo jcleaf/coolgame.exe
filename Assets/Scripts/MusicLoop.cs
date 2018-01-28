e34c3f29335454bd8bf6ad724d3ee405ac2c078c
Jonathan Leaf <jonathancleaf@gmail.com>
2018-01-28 12:15:41 -0800

    make music and progression persistent objects

--- Assets/Scripts/MusicLoop.cs
+++ Assets/Scripts/MusicLoop.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class MusicLoop : MonoBehaviour {
 
+	private static MusicLoop instanceRef;
 	public AudioSource Src;
 	int Step;
 	public float maxTime;
 	public float minTime;
 	// Use this for initialization
 	void Start () {
+		if(instanceRef == null)
+		{
+			instanceRef = this;
+			DontDestroyOnLoad(gameObject);
+		}else
+		{
+			DestroyImmediate(gameObject);
+		}
+
 		Src = GetComponent<AudioSource> ();
 	}
+
+
+
 
 	private void LateUpdate()
 	{
