8db3f490adc26596ba11775268678fb1636722e3
Caleb Markley <cablay91@gmail.com>
2018-01-28 10:28:47 -0800

    Allow restart level on death.

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    public IntReference playerHealth;
+
+	void Update ()
+    {
+        if ((Input.GetButtonDown("Submit") || Input.GetButtonDown("Reset")) && playerHealth <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
