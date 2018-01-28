da77b35efe2a9d25cf6906d8320c27ab7c764f92
Caleb Markley <cablay91@gmail.com>
2018-01-28 00:50:31 -0800

    Background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private MeshRenderer meshRenderer;
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 posOffset = transform.position - originalPosition;
+        Vector2 offset = new Vector2(posOffset.x, posOffset.z) * scrollSpeed;
+        meshRenderer.material.SetTextureOffset("_MainTex", offset);
+    }
+}
