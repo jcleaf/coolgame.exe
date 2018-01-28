d06fdf209ff725cdce9ae445becb2a64f4240b1a
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-01-27 23:12:01 -0800

    Fix up the graphics clipping.
    Few different ways to do this, but I went with the one that seemed to work

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour {
-	
+
+    [SerializeField]
+    [Tooltip("How much to move the sprite forward (to avoid clipping).")]
+    private float Offset;
+
+    private Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos = transform.localPosition;
+    }
+
 	void Update () {
-        transform.parent.LookAt(transform.parent.position + UnityEngine.Camera.main.transform.rotation * Vector3.forward,
+        transform.LookAt(transform.position + UnityEngine.Camera.main.transform.rotation * Vector3.forward,
                     UnityEngine.Camera.main.transform.rotation * Vector3.up);
+        Vector3 dir = new Vector3(transform.up.x, 0, transform.up.z);
+        transform.localPosition = StartPos - Offset * dir;
 	}
 }
