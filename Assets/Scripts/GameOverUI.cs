d3bdddb0b4ba0224431cd163c37cd429b942dfd0
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-01-28 14:59:32 -0800

    Make Game over fade in :)

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -9,6 +9,8 @@ public class GameOverUI : MonoBehaviour
     private Image goBackground;
     private Text[] texts;
 
+    private float graphicsCount = -1;
+
     void Awake()
     {
         goBackground = GetComponentInChildren<Image>();
@@ -17,7 +19,30 @@ public class GameOverUI : MonoBehaviour
 
     void Update()
     {
-        goBackground.enabled = playerHealth <= 0 && !playerWon;
-        foreach (Text text in texts) { text.enabled = playerHealth <= 0 && !playerWon; }
+        var done = (playerHealth <= 0 && !playerWon);
+        goBackground.enabled = done;
+        foreach (Text text in texts)
+        {
+            text.enabled = done;
+        }
+
+        if (done) {
+            graphicsCount += 0.5f * Time.deltaTime;
+            if (graphicsCount > 1)
+            {
+                graphicsCount = 1;
+            }
+
+            var c = goBackground.color;
+            c.a = 0.5f * graphicsCount;
+            goBackground.color = c;
+
+            foreach (Text text in texts)
+            {
+                var c2 = text.color;
+                c2.a = 1f * graphicsCount;
+                text.color = c2;
+            }
+        }
     }
 }
